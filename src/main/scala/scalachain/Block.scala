150b11fcd6327062b7492f7538d9e612d2e04a9c
anshul <anshulbhatnagar93@gmail.com>
2017-10-11 17:42:17 +0530

    validate each block

--- src/main/scala/scalachain/Block.scala
+++ src/main/scala/scalachain/Block.scala
@@ -1,8 +1,43 @@
-/**
-  * Created by yankee on 5/10/17.
-  */
+
 package scalachain
 
-class Block(val index:Long, val previousHash:String, val timestamp: String, val data:Map[String,Long], val hash:String) {
+import scala.annotation.tailrec
+
+case class Block(index:Long, previousHash:String, timestamp: Long, data:Map[String,Long], hash:String)
+
+
+object Block{
+  def apply()=List(GenesisBlock)
+
+
+  def validateBlock(newBlock:Block,previousBlock:Block) ={
+    previousBlock.index + 1 == newBlock.index &&
+      previousBlock.hash == newBlock.previousHash &&
+      HashCalculator(newBlock.index,newBlock.previousHash,newBlock.timestamp,newBlock.data) == newBlock.hash
+  }
+
+
+  @tailrec
+  def validateChain( chain: List[Block] ): Boolean = chain match {
+    case singleBlock :: Nil if singleBlock == GenesisBlock => true
+    case head :: mid :: tail if validateBlock(head, mid) => validateChain(mid :: tail)
+    case _ => false
+  }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
